b065877a42536c1eede3c46794380e6633bcfa24
hiago santos <foto.hiago@gmail.com>
2019-10-18 15:33:15 -0300

    react

---
+++ placarcontainer.js
@@ -0,0 +1,8 @@
+import React from "react"
+import { directive } from "@babel/types"
+
+cont placarcontainer = () => {
+    return(
+    <div className= "app"
+    PlacarContainer
+    
--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './ Placar.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Placar = ({time}) =>{
+    const [votos,setVotos] = useState(0);
+ 
+    const incrementar = () =>{
+      setVotos(votos +1);
+    }
 }
 
-export default App;
+const decrementar = () =>{
+  if(votos >0){
+    setVotos(votos -1);
+  }
+}
+
+export default Placarcontainer;
