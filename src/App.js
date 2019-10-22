8643998f8cb1f7e243bb61f601f47e7941aefbce
Karen Queiroz <kqueiroz812@gmail.com>
2019-10-22 15:24:24 -0300

    criar perfil doador

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Login from './componentes/Login/Login';
 import Escola from './componentes/Cadastro/Escola';
 import Doador from './componentes/Cadastro/Doador';
 import PerfilEscola from './componentes/PerfilEscola/PerfilEscola';
+import PerfilDoador from './componentes/PerfilDoador/PerfilDoador';
 
 
 import {BrowserRouter as Router,Route} from 'react-router-dom';
@@ -14,10 +15,11 @@ const App = () => {
     <div>
       <Router>
        <Route exact path="/" component={Home}/>
-       <Route exact path="/login" component={Login}/>
-       <Route exact path="/escola" component={Escola}/>
-       <Route exact path="/doador" component={Doador}/>
-       <Route exact path="/perfilescola" component={PerfilEscola}/>
+       <Route  path="/login" component={Login}/>
+       <Route  path="/escola" component={Escola}/>
+       <Route  path="/doador" component={Doador}/>
+       <Route  path="/perfilescola" component={PerfilEscola}/>
+       <Route  path="/perfildoador" component={PerfilDoador}/>
 
       </Router>
 
---
+++ src/componentes/PerfilDoador/PerfilDoador.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import './PerfilDoador.scss'
+import Botao from '../Botao/Botao';
+
+const PerfilDoador = () => {
+    return (
+        <div className="PerfilDoador">
+            <div className="perfil">
+                <h5>perfil</h5>
+            </div>
+            <section className="infos">
+                <div>
+
+                <h6>Nome:</h6>
+                <p>Joãozinho</p>
+                <h6>Email:</h6>
+                <h6>Idade:</h6>
+                <p>23</p>
+                <p>joao@sesc.com</p>
+                <h6>Telefone:</h6>
+                <p>11545302053</p>
+                <h6>Endereço:</h6>
+                <p>Rua da cacimba</p>
+                </div>
+                <Botao>Cadastrar Item</Botao>
+            </section>
+        </div>
+    )
+}
+
+export default PerfilDoador;
